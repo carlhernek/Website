048733e84e6e9eced9f9e18a03fcf087e1585174
carlhernek <53989735+carlhernek@users.noreply.github.com>
2019-11-17 20:55:10 +0100

    Animated button debug

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ let spanaOne = document.getElementById("icon-one-1");
 let spanbOne = document.getElementById("icon-two-1");
 
 let circle = "buttonicon-circle-i";
+let hidden = "buttonicon-hidden-i"
 let lineI = "buttonicon-line-90-i";
 let lineMerge = "buttonicon-line-90";
 let line45 = "buttonicon-line-45";
@@ -14,14 +15,14 @@ function transformToX () {
         spanaOne.classList.add(lineMerge);
         spanbOne.classList.remove(lineI);
         spanbOne.classList.add(lineMerge);
-    }, 500)
+    }, 100)
 
     setTimeout(function(){
         spanaOne.classList.remove(lineMerge);
         spanaOne.classList.add(line45);
         spanbOne.classList.remove(lineMerge);
         spanbOne.classList.add(lineN45);
-    }, 1000)
+    }, 700)
 }
 
 function transformToI () {
@@ -30,14 +31,19 @@ function transformToI () {
         spanaOne.classList.add(lineMerge);
         spanbOne.classList.remove(lineN45);
         spanbOne.classList.add(lineMerge);
-    }, 500)
+    }, 100)
 
     setTimeout(function(){
         spanaOne.classList.remove(lineMerge);
+        spanaOne.classList.add(hidden);
+    }, 500)
+
+    setTimeout(function(){
+        spanaOne.classList.remove(hidden);
         spanaOne.classList.add(circle);
         spanbOne.classList.remove(lineMerge);
         spanbOne.classList.add(lineI);
-    }, 1000)
+    }, 800)
 }
 
 spanaOne.classList.add(circle);
