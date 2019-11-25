84b0c07a47ace3a390953e52a75ff02578a74353
carlhernek <53989735+carlhernek@users.noreply.github.com>
2019-11-25 16:34:33 +0100

    Project Button Done

--- js/buttonanim.js
+++ js/buttonanim.js
@@ -1,6 +1,10 @@
 // Code for button animation
 let spanaOne = document.getElementById("icon-one-1");
 let spanbOne = document.getElementById("icon-two-1");
+let spanaTwo = document.getElementById("icon-one-2");
+let spanbTwo = document.getElementById("icon-two-2");
+let spanaThree = document.getElementById("icon-one-3");
+let spanbThree = document.getElementById("icon-two-3");
 
 let circle = "buttonicon-circle-i";
 let hidden = "buttonicon-hidden-i"
@@ -10,7 +14,9 @@ let lineMerge = "buttonicon-line-90";
 let line45 = "buttonicon-line-45";
 let lineN45 = "buttonicon-line--45";
 
-function transformToX () {
+// Function button One
+
+function transformOneToX () {
     setTimeout(function(){
         spanaOne.classList.remove(circle);
         spanaOne.classList.add(lineMerge);
@@ -26,7 +32,7 @@ function transformToX () {
     }, 700)
 }
 
-function transformToI () {
+function transformOneToI () {
     setTimeout(function(){
         spanaOne.classList.remove(line45);
         spanaOne.classList.add(lineMerge);
@@ -52,6 +58,100 @@ function transformToI () {
     }, 1000)
 }
 
+// Function button Two
+
+function transformTwoToX () {
+    setTimeout(function(){
+        spanaTwo.classList.remove(circle);
+        spanaTwo.classList.add(lineMerge);
+        spanbTwo.classList.remove(lineI);
+        spanbTwo.classList.add(lineMerge);
+    }, 100)
+
+    setTimeout(function(){
+        spanaTwo.classList.remove(lineMerge);
+        spanaTwo.classList.add(line45);
+        spanbTwo.classList.remove(lineMerge);
+        spanbTwo.classList.add(lineN45);
+    }, 700)
+}
+
+function transformTwoToI () {
+    setTimeout(function(){
+        spanaTwo.classList.remove(line45);
+        spanaTwo.classList.add(lineMerge);
+        spanbTwo.classList.remove(lineN45);
+        spanbTwo.classList.add(lineMerge);
+    }, 100)
+
+    setTimeout(function(){
+        spanaTwo.classList.remove(lineMerge);
+        spanaTwo.classList.add(hiddenMerge);
+    }, 700)
+
+    setTimeout(function(){
+        spanaTwo.classList.remove(hiddenMerge);
+        spanaTwo.classList.add(hidden);
+    }, 850)
+
+    setTimeout(function(){
+        spanaTwo.classList.remove(hidden);
+        spanaTwo.classList.add(circle);
+        spanbTwo.classList.remove(lineMerge);
+        spanbTwo.classList.add(lineI);
+    }, 1000)
+}
+
+// Function button Three
+
+function transformThreeToX () {
+    setTimeout(function(){
+        spanaThree.classList.remove(circle);
+        spanaThree.classList.add(lineMerge);
+        spanbThree.classList.remove(lineI);
+        spanbThree.classList.add(lineMerge);
+    }, 100)
+
+    setTimeout(function(){
+        spanaThree.classList.remove(lineMerge);
+        spanaThree.classList.add(line45);
+        spanbThree.classList.remove(lineMerge);
+        spanbThree.classList.add(lineN45);
+    }, 700)
+}
+
+function transformThreeToI () {
+    setTimeout(function(){
+        spanaThree.classList.remove(line45);
+        spanaThree.classList.add(lineMerge);
+        spanbThree.classList.remove(lineN45);
+        spanbThree.classList.add(lineMerge);
+    }, 100)
+
+    setTimeout(function(){
+        spanaThree.classList.remove(lineMerge);
+        spanaThree.classList.add(hiddenMerge);
+    }, 700)
+
+    setTimeout(function(){
+        spanaThree.classList.remove(hiddenMerge);
+        spanaThree.classList.add(hidden);
+    }, 850)
+
+    setTimeout(function(){
+        spanaThree.classList.remove(hidden);
+        spanaThree.classList.add(circle);
+        spanbThree.classList.remove(lineMerge);
+        spanbThree.classList.add(lineI);
+    }, 1000)
+}
+
 spanaOne.classList.add(circle);
 spanbOne.classList.add(lineI);
+
+spanaTwo.classList.add(circle);
+spanbTwo.classList.add(lineI);
+
+spanaThree.classList.add(circle);
+spanbThree.classList.add(lineI);
 
--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ btnOne.onclick = function() {
     switch(eOne) {
 
     case 1:
-        transformToX();
+        transformOneToX();
         contOne.style.transition = "1s" // To avoid the div crawling along the screen when flipping phone
         contOne.classList.remove("infocontainer");
         contOne.classList.add("infocontainer-show");
@@ -24,7 +24,7 @@ btnOne.onclick = function() {
         break;
     
     case 2:
-        transformToI();
+        transformOneToI();
         contOne.style.transition = "1s" 
         contOne.classList.remove("infocontainer-show");
         contOne.classList.add("infocontainer");
@@ -33,6 +33,84 @@ btnOne.onclick = function() {
         infoOne.style.opacity = "0"
         eOne = 1
         setTimeout(function(){contOne.style.transition = "0s"}, 1000)
+        break;
+    }
+}
+
+// Code for project 2
+
+let btnTwo = document.getElementById("infobutton-2");
+let contTwo = document.getElementById("infocontainer-2");
+let infoTwo = document.getElementById("infohandler-2");
+let eTwo = 1;
+
+btnTwo.onclick = function() {
+
+    switch(eTwo) {
+
+    case 1:
+        transformTwoToX();
+        contTwo.style.transition = "1s" 
+        contTwo.classList.remove("infocontainer");
+        contTwo.classList.add("infocontainer-show");
+        btnTwo.style.opacity = "1";
+        btnTwo.style.bottom = "-30px";
+        setTimeout(function(){
+            infoTwo.style.opacity = "1";
+            contTwo.style.transition = "0s";
+        }, 1000)
+        eTwo = 2
+        break;
+    
+    case 2:
+        transformTwoToI();
+        contTwo.style.transition = "1s" 
+        contTwo.classList.remove("infocontainer-show");
+        contTwo.classList.add("infocontainer");
+        btnTwo.style.opacity = "0.7";
+        btnTwo.style.bottom = "3vh";
+        infoTwo.style.opacity = "0"
+        eTwo = 1
+        setTimeout(function(){contTwo.style.transition = "0s"}, 1000)
+        break;
+    }
+}
+
+// Code for project 3
+
+let btnThree = document.getElementById("infobutton-3");
+let contThree = document.getElementById("infocontainer-3");
+let infoThree = document.getElementById("infohandler-3");
+let eThree = 1;
+
+btnThree.onclick = function() {
+
+    switch(eThree) {
+
+    case 1:
+        transformThreeToX();
+        contThree.style.transition = "1s" 
+        contThree.classList.remove("infocontainer");
+        contThree.classList.add("infocontainer-show");
+        btnThree.style.opacity = "1";
+        btnThree.style.bottom = "-30px";
+        setTimeout(function(){
+            infoThree.style.opacity = "1";
+            contThree.style.transition = "0s";
+        }, 1000)
+        eThree = 2
+        break;
+    
+    case 2:
+        transformThreeToI();
+        contThree.style.transition = "1s" 
+        contThree.classList.remove("infocontainer-show");
+        contThree.classList.add("infocontainer");
+        btnThree.style.opacity = "0.7";
+        btnThree.style.bottom = "3vh";
+        infoThree.style.opacity = "0"
+        eThree = 1
+        setTimeout(function(){contThree.style.transition = "0s"}, 1000)
         break;
     }
 }
