272d1c821e8a75ee5406222b569ba0ca954c60c6
carlhernek <53989735+carlhernek@users.noreply.github.com>
2019-11-26 11:54:10 +0100

    Updates to menu

--- js/desktopmenu.js
+++ js/desktopmenu.js
@@ -3,10 +3,30 @@ let scrOne = document.getElementById("aboutme");
 let scrTwo = document.getElementById("project-1");
 let scrThree = document.getElementById("project-2");
 let scrFour = document.getElementById("project-3");
-let scrFive = document.getElementById("");
+let scrFive = document.getElementById("curriculumv");
 
+let menuBtnOne = document.getElementById("about");
+let menuBtnTwo = document.getElementById("frontend");
+let menuBtnThree = document.getElementById("uiux");
+let menuBtnFour = document.getElementById("design");
+let menuBtnFive = document.getElementById("resume");
 
-function panOne() {
+menuBtnOne.onclick = function () {
     scrOne.scrollIntoView();
 }
 
+menuBtnTwo.onclick = function () {
+    scrTwo.scrollIntoView();
+}
+
+menuBtnThree.onclick = function () {
+    scrThree.scrollIntoView();
+}
+
+menuBtnFour.onclick = function () {
+    scrFour.scrollIntoView();
+}
+
+menuBtnFive.onclick = function () {
+    scrFive.scrollIntoView();
+}
